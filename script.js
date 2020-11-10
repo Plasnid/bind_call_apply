460e414c879d9c2fa840eb5d986e65ac03a99b08
Plasnid <stuart@sackler.ca>
2020-11-10 12:29:55 -0500

    examples of bind call and apply added

---
+++ script.js
@@ -0,0 +1,41 @@
+let decidiousPlant1 = {
+    plantName: "Japanese Maple",
+    foliageType: "leaves",
+    foliageColour: "red"
+}
+
+let decidiousPlant2 = {
+    plantName: "Weeping willow",
+    foliageType: "leaves",
+    foliageColour: "light green"
+}
+let decidiousPlant3 = {
+    plantName: "Cactus",
+    foliageType: "spines",
+    foliageColour: "brown"
+}
+
+function talkAboutTrees(expert){
+    console.log(this);
+    return(`${expert} says: a ${this.plantName} has ${this.foliageColour} ${this.foliageType}`);
+}
+
+console.log(talkAboutTrees());
+
+// * now with bind
+// * bind returns a function that is bound to an object or scope
+// * we call that function as we would call the original function
+let bindExample = talkAboutTrees.bind(decidiousPlant1);
+console.log(bindExample("Horticulturalist"));
+
+// * now with call
+// * call does not wrap the original function in a new function
+// * the struture looks like this
+// * functionName.call(scopeToBindTo,commaSeparatedParameters)
+console.log(talkAboutTrees.call(decidiousPlant2, "Horticulturist"));
+
+// * now with apply
+// * apply also does not wrap the original function in a new funtion
+// * the structure looks like this
+// * functionName.apply(scopeToBindTo,[array of parameters]);
+console.log(talkAboutTrees.apply(decidiousPlant3,["Horticulturist"]));
